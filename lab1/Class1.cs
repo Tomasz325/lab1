ea8e9c8962d458af77ef6a399413d5be7c2280c8
Tomasz Szpil <Tomasz.Szpil@microsoft.wsei.edu.pl>
2022-03-05 14:08:17 +0100

    Dodaj pliki projektów.

---
+++ lab1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Ulamek
+    {
+        private int licznik;
+        private int mianownik;
+        public Ulamek()
+        {
+            int licznik = 0;
+            int mianownik = 1;
+        }
+        public Ulamek(int licznik, int mianownik)
+        {
+            this.licznik = licznik;
+            this.mianownik = mianownik;
+        }
+        public Ulamek(Ulamek perviousUlamek)
+        {
+            licznik = perviousUlamek.licznik;
+            mianownik = perviousUlamek.mianownik;
+        }
+        override public object ToString(int licznik, int mianowink)
+        {
+            return mianownik. + licznik.ToString();
+        }
+    }
+
+}
